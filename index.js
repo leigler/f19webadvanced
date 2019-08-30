a00d32c219e8b4ad923c041c95cfc17e3a23d08e
Lukas Eigler-Harding <eiglerhardingl@gmail.com>
2019-08-30 10:47:50 -0400

    updated syllabus link

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ window.onload = function(){
 		{ 
 			day: "Friday, August 30",
 			list: [
-				"Introductions, <a target='_blank' href='https://arena-attachments.s3.amazonaws.com/4925364/6282368f5ad5a6b105099135fee208f3.pdf' >Syllabus Review</a>, Expectations", 
+				"Introductions, <a target='_blank' href='https://arena-attachments.s3.amazonaws.com/4925664/a211189c7746cf80e8af048666ca405d.pdf' >Syllabus Review</a>, Expectations", 
 				"Discussion: HTML/CSS Review",
 				"Github installation, <a target='_blank' href='https://git-scm.com/book/en/v2/Getting-Started-Installing-Git'>Git</a>, <a target='_blank' href='https://help.github.com/en/desktop/getting-started-with-github-desktop'>Github Desktop</a>, and <a target='_blank' href='https://pages.github.com/'>Github Pages</a>"
 			],
@@ -15,10 +15,9 @@ window.onload = function(){
 				"Reading: <a target='_blank' href='https://thecreativeindependent.com/people/laurel-schwulst-my-website-is-a-shifting-house-next-to-a-river-of-knowledge-what-could-yours-be/'>My Website is a shifting house next to a river of knowledge. What could yours be?</a> by Laurel Schwulst",
 				"Take the Github page that you set up in today’s class and convert it into a growing index of the work you will do for this class. As you design your index, consider what information is attached to each item (this might include the project title, date, brief, description, or screenshots), and what kind of information you want to include on the periphery (how is the site introduced? How do we know to whom the site belongs to?).",
 				"Think about how this index will grow and change in reaction to the projects you will add. Your index should be built responsively (work on desktop and mobile). The goal of this 1-week assignment is to build a dynamic system using only HTML/CSS."
-				]
-		
+				]		
 		}
-	]
+	];
 
 	contents.forEach(function(content){
 		document.querySelector("#root").innerHTML += `${Site.basicContent(content)}`
